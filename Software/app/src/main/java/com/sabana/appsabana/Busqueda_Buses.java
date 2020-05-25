75e3812ed3c989d9685e35d0022d1b941b17265f
Bayunas <bayunas12@gmail.com>
2020-05-24 19:47:53 -0500

    Interfaz

---
+++ Software/app/src/main/java/com/sabana/appsabana/Busqueda_Buses.java
@@ -0,0 +1,69 @@
+package com.sabana.appsabana;
+
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+
+public class Busqueda_Buses extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    TextView mTimeView;
+    Button MpickTime;
+    Context mContext=this;
+    Button regresar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busqueda__buses);
+
+        regresar = (Button) findViewById(R.id.regresar);
+
+        regresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Busqueda_Buses.this, MainActivity.class));
+            }
+        });
+
+        Spinner spinner =findViewById(R.id.spinnerBus);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.Destinos, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        mTimeView =(TextView) findViewById(R.id.VistaHora);
+        Calendar calendar= Calendar.getInstance();
+        final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        final int minute = calendar.get(Calendar.MINUTE);
+
+        MpickTime =(Button) findViewById(R.id.Tiempo);
+        MpickTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(mContext, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        mTimeView.setText(hourOfDay+ ":" + minute);
+                    }
+                },hour,minute,android.text.format.DateFormat.is24HourFormat(mContext));
+                timePickerDialog.show();
+            }
+        });
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text= parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
