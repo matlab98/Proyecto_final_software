8787c5a48e0efa1447fc76f1f7ad937822c0f9e8
Bayunas <bayunas12@gmail.com>
2020-05-25 21:18:59 -0500

    Interfaz Avance

--- Software/app/src/main/java/com/sabana/appsabana/MainMenu.java
+++ Software/app/src/main/java/com/sabana/appsabana/MainMenu.java
@@ -10,10 +10,20 @@ import android.os.Bundle;
 public class MainMenu extends AppCompatActivity {
    Button salirlogin;
    Button Buses;
+   Button Tren;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        Tren= (Button)findViewById(R.id.tren);
+        Tren.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenu.this, BusesGeneral.class));
+            }
+        });
+
 
         Buses=(Button)findViewById(R.id.bus);
         Buses.setOnClickListener(new View.OnClickListener() {
