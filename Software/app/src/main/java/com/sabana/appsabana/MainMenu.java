6293c0ab8bbe8766cdc766cf454494a19085d593
Bayunas <bayunas12@gmail.com>
2020-05-28 11:14:21 -0500

    Cambio main menu

--- Software/app/src/main/java/com/sabana/appsabana/MainMenu.java
+++ Software/app/src/main/java/com/sabana/appsabana/MainMenu.java
@@ -21,6 +21,7 @@ public class MainMenu extends AppCompatActivity {
         init();
     }
 
+
     private void init() {
         Trense = findViewById(R.id.tren);
         Trense.setOnClickListener(new View.OnClickListener() {
